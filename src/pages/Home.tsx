502e299546da8165ace3a19fb3e96282a7592c7a
kimywann <Kimywan10@gmail.com>
2024-11-20 17:03:29 +0900

    feat: 테마 기반 Title 컴포넌트 구현 및 스타일 정의

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import Title from "../components/common/Title";
+
 function Home() {
     return (
         <>
+        <Title size="medium" color="background">
+            제목 텍스트
+        </Title>
             <div>home body</div>
         </>
     );
